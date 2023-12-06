c8d4457c06cbf44c4bdd2e3267d24a7a8fab5e89
truongkhongsai <anhp31746@gmail.com>
2023-12-06 21:59:06 +0700

    New

---
+++ Untitled6.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int Untitled6 () {
+    string yourOpinion;
+
+   
+    cout << "G?i ? ki?n c?a b?n: ";
+    getline(cin, yourOpinion);
+
+  
+    cout << "? ki?n c?a b?n ð? ðý?c g?i thành công!" << endl;
+    cout << "C?m õn b?n ð? chia s? ? ki?n c?a m?nh." << endl;
+
+    return 0;
+}
+
